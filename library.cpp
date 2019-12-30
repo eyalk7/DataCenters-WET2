8e971fbc88679d6f47e8fa35dd844c123c600feb
DaniRabi <danirabi98@gmail.com>
2019-12-30 18:24:35 +0200

    library offset

--- library.cpp
+++ library.cpp
@@ -16,7 +16,7 @@ StatusType MergeDataCenters(void *DS, int dataCenter1, int dataCenter2) {
     if (!DS || dataCenter1 <= 0 || dataCenter2 <= 0) return INVALID_INPUT;
     auto manager = (DataCentersManager*)DS;
     try {
-        return (StatusType)(manager->MergeDataCenters(dataCenter1, dataCenter2));
+        return (StatusType)(manager->MergeDataCenters(dataCenter1 - 1, dataCenter2 - 1));
     } catch (std::bad_alloc& ba) {
         return ALLOCATION_ERROR;
     }
@@ -26,7 +26,7 @@ StatusType AddServer(void *DS, int dataCenterID, int serverID) {
     if (!DS || dataCenterID <= 0 || serverID <= 0) return INVALID_INPUT;
     auto manager = (DataCentersManager*)DS;
     try {
-        return (StatusType)(manager->AddServer(dataCenterID, serverID));
+        return (StatusType)(manager->AddServer(dataCenterID - 1, serverID));
     } catch (std::bad_alloc& ba) {
         return ALLOCATION_ERROR;
     }
@@ -51,7 +51,7 @@ StatusType SetTraffic(void *DS, int serverID,  int traffic) {
 StatusType SumHighestTrafficServers(void *DS, int dataCenterID,  int k, int *traffic) {
     if (!DS || dataCenterID < 0 || k < 0 || !traffic) return INVALID_INPUT;
     auto manager = (DataCentersManager*)DS;
-    return (StatusType)(manager->SumHighestTrafficServers(dataCenterID, k, traffic));
+    return (StatusType)(manager->SumHighestTrafficServers(dataCenterID - 1, k, traffic));
 }
 
 void Quit(void** DS) {
